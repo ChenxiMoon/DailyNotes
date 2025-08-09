c5636dcf0da0dcfc84aa3f3c8e090ff7ec186670
ChenxiMoon <2403133073@qq.com>
2025-08-09 13:22:54 +0800

    docs: 自动更新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,16 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Author: ChenxiMoon 2403133073@qq.com
  * @Date: 2025-08-08 22:54:29
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-08-08 22:55:10
+ * @LastEditors: ChenxiMoon 2403133073@qq.com
+ * @LastEditTime: 2025-08-09 13:22:39
  * @FilePath: \undefinedf:\Zmk\DailyNotes\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { defineUserConfig, defaultTheme } from 'vuepress'
+import { defineUserConfig } from 'vuepress'
+import themeDefaultPkg from '@vuepress/theme-default'
 import { viteBundler } from '@vuepress/bundler-vite'
+
+const defaultTheme = themeDefaultPkg.default || themeDefaultPkg
 
 export default defineUserConfig({
   lang: 'zh-CN',
@@ -15,8 +18,15 @@ export default defineUserConfig({
   description: '每日学习记录',
 
   theme: defaultTheme({
-    navbar: [],
-    sidebar: [],
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about/' },
+    ],
+    sidebar: [
+      '/',
+      '/guide/',
+      '/about/',
+    ],
   }),
 
   bundler: viteBundler(),
